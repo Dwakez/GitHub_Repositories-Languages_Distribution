37e6e127e93aeec094e8a4468d168a96e294da60
์Nichapat Kachacheewa <42914563+khunpanggg@users.noreply.github.com>
2018-11-29 16:55:53 +0700

    Shorten Code

--- 01. Create_Score_Distribution_CSV.py
+++ 01. Create_Score_Distribution_CSV.py
@@ -9,77 +9,49 @@ def main():
 
     # Convert Data to list
     # Mathematics Scores Distribution
-    math_10th_pct = df.loc[23].tolist()
-    math_25th_pct = df.loc[24].tolist()
-    math_50th_pct = df.loc[25].tolist()
-    math_05th_pct = df.loc[26].tolist()
-    math_75th_pct = df.loc[27].tolist()
-    math_90th_pct = df.loc[28].tolist()
-    math_95th_pct = df.loc[29].tolist()
+    math_pct_list = [df.loc[i].tolist() for i in [26, *range(23, 26), *range(27, 30)]]
 
     # Reading Scores Distribution
-    read_10th_pct = df.loc[30].tolist()
-    read_25th_pct = df.loc[31].tolist()
-    read_50th_pct = df.loc[32].tolist()
-    read_05th_pct = df.loc[33].tolist()
-    read_75th_pct = df.loc[34].tolist()
-    read_90th_pct = df.loc[35].tolist()
-    read_95th_pct = df.loc[36].tolist()
+    read_pct_list = [df.loc[i].tolist() for i in [33, *range(30, 33), *range(34, 37)]]
 
     # Science Scores Distribution
-    sci_10th_pct = df.loc[37].tolist()
-    sci_25th_pct = df.loc[38].tolist()
-    sci_50th_pct = df.loc[39].tolist()
-    sci_05th_pct = df.loc[40].tolist()
-    sci_75th_pct = df.loc[41].tolist()
-    sci_90th_pct = df.loc[42].tolist()
-    sci_95th_pct = df.loc[43].tolist()
+    sci_pct_list = [df.loc[i].tolist() for i in [40, *range(37, 40), *range(41, 44)]]
 
     print("Sucessfully converted data")
 
     # Store data in dictionary
-    math_dis_data = {
-        math_05th_pct[2]: [math_05th_pct[i] for i in range(4, 20, 3)],
-        math_10th_pct[2]: [math_10th_pct[i] for i in range(4, 20, 3)],
-        math_25th_pct[2]: [math_25th_pct[i] for i in range(4, 20, 3)],
-        math_50th_pct[2]: [math_50th_pct[i] for i in range(4, 20, 3)],
-        math_75th_pct[2]: [math_75th_pct[i] for i in range(4, 20, 3)],
-        math_90th_pct[2]: [math_90th_pct[i] for i in range(4, 20, 3)],
-        math_95th_pct[2]: [math_95th_pct[i] for i in range(4, 20, 3)]
-    }
+    math_dis_data = {}
+    read_dis_data = {}
+    sci_dis_data = {}
 
-    read_dis_data = {
-        read_05th_pct[2]: [read_05th_pct[i] for i in range(4, 20, 3)],
-        read_10th_pct[2]: [read_10th_pct[i] for i in range(4, 20, 3)],
-        read_25th_pct[2]: [read_25th_pct[i] for i in range(4, 20, 3)],
-        read_50th_pct[2]: [read_50th_pct[i] for i in range(4, 20, 3)],
-        read_75th_pct[2]: [read_75th_pct[i] for i in range(4, 20, 3)],
-        read_90th_pct[2]: [read_90th_pct[i] for i in range(4, 20, 3)],
-        read_95th_pct[2]: [read_95th_pct[i] for i in range(4, 20, 3)]
-    }
+    math_key = [math_pct_list[i][2] for i in range(7)]
+    for i in math_key:
+        math_dis_data[i] = [math_pct_list[math_key.index(i)][j] for j in range(4, 20, 3)]
 
-    sci_dis_data = {
-        sci_05th_pct[2]: [sci_05th_pct[i] for i in range(4, 20, 3)],
-        sci_10th_pct[2]: [sci_10th_pct[i] for i in range(4, 20, 3)],
-        sci_25th_pct[2]: [sci_25th_pct[i] for i in range(4, 20, 3)],
-        sci_50th_pct[2]: [sci_50th_pct[i] for i in range(4, 20, 3)],
-        sci_75th_pct[2]: [sci_75th_pct[i] for i in range(4, 20, 3)],
-        sci_90th_pct[2]: [sci_90th_pct[i] for i in range(4, 20, 3)],
-        sci_95th_pct[2]: [sci_95th_pct[i] for i in range(4, 20, 3)]
-    }
+    read_key = [read_pct_list[i][2] for i in range(7)]
+    for i in read_key:
+        read_dis_data[i] = [read_pct_list[read_key.index(i)][j] for j in range(4, 20, 3)]
 
+    sci_key = [read_pct_list[i][2] for i in range(7)]
+    for i in sci_key:
+        sci_dis_data[i] = [sci_pct_list[sci_key.index(i)][j] for j in range(4, 20, 3)]
+
+    # List of Values in Dictionaries
     math_values_list = list(math_dis_data.values())
     read_values_list = list(read_dis_data.values())
     sci_values_list = list(sci_dis_data.values())
+    # Value List
     math_list = [i for i in math_values_list]
     read_list = [i for i in read_values_list]
     sci_list = [i for i in sci_values_list]
-    create_csv("[Score Distribution] - Mathematics", list(math_dis_data.keys()), math_dis_data, math_list)
-    create_csv("[Score Distribution] - Reading", list(read_dis_data.keys()), read_dis_data, read_list)
-    create_csv("[Score Distribution] - Science", list(sci_dis_data.keys()), sci_dis_data, sci_list)
+
+    # Create CSV
+    create_csv("[Score Distribution] - Mathematics", list(math_dis_data.keys()), math_list)
+    create_csv("[Score Distribution] - Reading", list(read_dis_data.keys()), read_list)
+    create_csv("[Score Distribution] - Science", list(sci_dis_data.keys()), sci_list)
     print("Sucessfully create CSV !!")
 
-def create_csv(name, key, dis_data, row_list):
+def create_csv(name, key, row_list):
     """ Create New CSV """
     new_file_name = name + ".csv"
     with open(new_file_name, "w", newline="") as new_file_name:
--- 02. Plot_Score_Distribution_SVG.py
+++ 02. Plot_Score_Distribution_SVG.py
@@ -18,7 +18,7 @@ def plot_graph(df, name):
     """ Plot SVG graph """
     year_list = list(map(str, range(2000, 2016, 3)))
     line_chart = pygal.Line()
-    line_chart.title = "Thailand PISA " + name.strip("[]") + " during 2000 - 2015"
+    line_chart.title = "Thailand PISA " + name.lstrip("[").replace("]", "") + " during 2000 - 2015"
     line_chart.x_labels = ["5th Pecentile", "10th Pecentile", "25th Pecentile", "50th Pecentile",
                            "75th Pecentile", "90th Pecentile", "95th Percentile"]
     for i in range(len(year_list)):
